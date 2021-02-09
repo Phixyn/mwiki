e196263c654b63cccd6898fc280693a82aefde32
phixyn <phixyn@gmail.com>
2021-02-09 23:55:47 +0000

    VuePress: Re-organize sidebar
    Re-order the categories displayed on the sidebar. This change makes it so that
    related topics are listed under each other, giving the sidebar a more sensible
    order.

--- content/.vuepress/config.js
+++ content/.vuepress/config.js
@@ -8,69 +8,6 @@ module.exports = {
         path: "/"
       },
       {
-        title: "Android",
-        children: [
-          "/android/android-bookmarks",
-          "/android/adb",
-          "/android/gradle"
-        ],
-      },
-      {
-        title: "Bookmarks",
-        children: [
-          "/bookmarks/dev-bookmarks",
-          "/bookmarks/qa-bookmarks",
-          "/bookmarks/tools-bookmarks",
-          "/bookmarks/ble-bookmarks"
-        ]
-      },
-      {
-        title: "CI/CD",
-        children: [
-          "/continuous-integration/jenkins-bookmarks",
-          "/continuous-integration/jenkins-pipeline"
-        ]
-      },
-      {
-        title: "Front-End Development",
-        children: [
-          "/front-end-development/vue-js",
-          "/front-end-development/gridsome",
-          "/front-end-development/react"
-        ]
-      },
-      {
-        title: "iOS",
-        children: [
-          "/ios/ios-bookmarks",
-          "/ios/xctest-cheatsheet"
-        ]
-      },
-      {
-        title: "Languages",
-        children: [
-          "/languages/korean-notes"
-        ]
-      },
-      {
-        title: "Machine Learning",
-        children: [
-          "/machine-learning/fundamentals",
-          "/machine-learning/models-and-algorithms",
-          "/machine-learning/tutorials",
-          "/machine-learning/statistics",
-          "/machine-learning/other",
-        ]
-      },
-      {
-        title: "MacOS",
-        children: [
-          "/macos/macos-bookmarks",
-          "/macos/macos-commands",
-          "/macos/macos-launchd"
-        ]
-      },
-      {
         title: "Programming",
         children: [
           "/programming/java-bookmarks",
@@ -85,9 +22,19 @@ module.exports = {
         ]
       },
       {
-        title: "SysAdmin",
+        title: "Front-End Development",
         children: [
-          "/sysadmin/nginx"
+          "/front-end-development/vue-js",
+          "/front-end-development/gridsome",
+          "/front-end-development/react"
+        ]
+      },
+      {
+        title: "Web Development",
+        children: [
+          "/web-development/foundation",
+          "/web-development/node-js",
+          "/web-development/web-dev-in-2020"
         ]
       },
       {
@@ -98,11 +45,64 @@ module.exports = {
         ]
       },
       {
-        title: "Web Development",
+        title: "CI/CD",
         children: [
-          "/web-development/foundation",
-          "/web-development/node-js",
-          "/web-development/web-dev-in-2020"
+          "/continuous-integration/jenkins-bookmarks",
+          "/continuous-integration/jenkins-pipeline"
+        ]
+      },
+      {
+        title: "SysAdmin",
+        children: [
+          "/sysadmin/nginx"
+        ]
+      },
+      {
+        title: "Android",
+        children: [
+          "/android/android-bookmarks",
+          "/android/adb",
+          "/android/gradle"
+        ],
+      },
+      {
+        title: "iOS",
+        children: [
+          "/ios/ios-bookmarks",
+          "/ios/xctest-cheatsheet"
+        ]
+      },
+      {
+        title: "MacOS",
+        children: [
+          "/macos/macos-bookmarks",
+          "/macos/macos-commands",
+          "/macos/macos-launchd"
+        ]
+      },
+      {
+        title: "Machine Learning",
+        children: [
+          "/machine-learning/fundamentals",
+          "/machine-learning/models-and-algorithms",
+          "/machine-learning/tutorials",
+          "/machine-learning/statistics",
+          "/machine-learning/other"
+        ]
+      },
+      {
+        title: "Bookmarks",
+        children: [
+          "/bookmarks/dev-bookmarks",
+          "/bookmarks/qa-bookmarks",
+          "/bookmarks/tools-bookmarks",
+          "/bookmarks/ble-bookmarks"
+        ]
+      },
+      {
+        title: "Languages",
+        children: [
+          "/languages/korean-notes"
         ]
       }
     ]
